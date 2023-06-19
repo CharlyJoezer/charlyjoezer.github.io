7f48bde0f4fae93b4972ec832e1008d83a32b5aa
Charly Joezer <charlyjoezer30@gmail.com>
2023-06-19 09:33:00 +0800

    Membuat fitur popup project

--- js/home.js
+++ js/home.js
@@ -184,5 +184,38 @@ $(function(){
 
 
 $('.button-see-project').click(function(){
-    window.location =  'https://github.com/CharlyJoezer'
+   const image = {
+    'cafe_order.png' : 'Order Kopi Online',
+    'skakes.jpg' : 'Pemeriksaan Laboratorium',
+    'PPM.jpg' : 'Pengaduan Masyarakat',
+    'toko_online.png' : 'Website E-Commerce '
+   };
+   const getKey = Object.keys(image)
+   const getValue = Object.values(image)
+   let imgActive = 0
+   $('#image-popup').attr('src', 'asset/'+getKey[imgActive])
+   $('#about-image').html(getValue[imgActive])
+   $('.bg-popup').css('display', 'block');
+   
+   $('.prev-btn').click(function(){
+       if(imgActive == 0){
+           return false;
+        }
+        $('#image-popup').attr('src', 'asset/'+getKey[imgActive - 1])
+        $('#about-image').html(getValue[imgActive - 1])
+        imgActive -= 1
+    })
+    $('.next-btn').click(function(){
+        if(imgActive == 3){
+            return false;
+        }
+        $('#image-popup').attr('src', 'asset/'+getKey[imgActive + 1])
+        $('#about-image').html(getValue[imgActive + 1])
+        imgActive += 1
+        
+   })
+
+   $('.close-popup').click(function(){
+        $('.bg-popup').css('display', 'none');
+   });
 });
