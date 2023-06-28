4c40bb805328a219854b87dcfcc1bca7bcec5638
CharlyJoezer <charlyjoezer30@gmail.com>
2023-06-28 11:07:04 +0800

    Modifikasi navbar mobile dan main text section 1

--- js/home.js
+++ js/home.js
@@ -2,18 +2,25 @@ $(document).scroll(function() {
     let scroll = $(window).scrollTop();
     if(scroll > 0){
         $('.navbar-desktop').addClass('bg-navbar-event-scroll')
+        $('.navbar-mobile').addClass('bg-navbar-event-scroll')
         $('.navbar-link, .navbar-header .image-and-text').addClass('navbar-link-event-scroll')
+        $('.toggle-navbar-items').addClass('navbar-link-event-scroll')
     }
     if(scroll == 0){
         $('.navbar-desktop').removeClass('bg-navbar-event-scroll')
+        $('.navbar-mobile').removeClass('bg-navbar-event-scroll')
         $('.navbar-link, .navbar-header .image-and-text').removeClass('navbar-link-event-scroll')
+        $('.toggle-navbar-items').removeClass('navbar-link-event-scroll')
     }
 })
 
 $('.toggle-navbar-items').click(function(){
     $('.navbar-mobile .navbar-items').toggleClass('show-navbar-items-mobile')
+    $('.navbar-mobile, .navbar-items').addClass('bg-navbar-event-scroll')
+    $('.navbar-link, .navbar-header .image-and-text').addClass('navbar-link-event-scroll')
+    $('.toggle-navbar-items').addClass('navbar-link-event-scroll')
 })
-$('.navbar-link').click(function(){
+$('.navbar-link, .button-navbar').click(function(){
     $('.navbar-mobile .navbar-items').removeClass('show-navbar-items-mobile')
 })
 $('.button-see-project').click(function(){
@@ -51,4 +58,18 @@ $('.button-see-project').click(function(){
    $('.close-popup').click(function(){
         $('.bg-popup').css('display', 'none');
    });
+});
+
+$(document).ready(function(){
+    const getDate = new Date();
+    const getHours = getDate.getHours();
+    if(getHours >= 0 && getHours < 12){
+        $('.header-text h1').html('Good Morning')
+    }else if(getHours >= 12 && getHours < 18){
+        $('.header-text h1').html('Good Afternoon')
+    }else if(getHours >= 18 && getHours < 24){
+        $('.header-text h1').html('Good Evening')
+    }else{
+        $('.header-text h1').html('Keep Grinding')
+    }
 });
